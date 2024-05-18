900271fd5be76b6a0b3902fb56fb5ca85f0a33ad
ZJUee <2077880746@qq.com>
2024-05-18 08:53:22 +0800

    Deployed  with MkDocs version: 1.6.0

---
+++ javascripts/mathjax.js
@@ -0,0 +1,19 @@
+window.MathJax = {
+    tex: {
+       inlineMath: [ ["\$","\$"] ],
+       inlineMath:[["\-\(","\)"]],
+       inlineMath: [ ["\\(","\\)"] ],
+       displayMath: [ ["\$\$","\$\$"] ],
+       displayMath:[[ "\\[", "\\]" ]],
+       processEscapes: true,
+       processEnvironments: true
+    },
+    options: {
+       ignoreHtmlClass: ".*|",
+       processHtmlClass: "arithmatex"
+    }
+    };
+  document$.subscribe(() => { 
+  MathJax.typesetPromise()
+  })
+  
